aa9e4b34c7bfb1bdf9acf81cde339ec975952a29
Cristiano7988 <cristiano.morales@contractor.outroll.com>
2023-12-13 01:07:48 -0300

    Criação das tabelas de Carros e Status

---
+++ database/migrations/2023_12_13_034726_create_carros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carros', function (Blueprint $table) {
+            $table->id();
+            $table->string('modelo');
+            $table->integer('ano');
+            $table->decimal('preco');
+            $table->foreignId('status_id')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carros');
+    }
+}
