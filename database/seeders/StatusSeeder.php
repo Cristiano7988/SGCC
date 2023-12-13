5eb02259430b65da10539d68ec2df26a45a88c02
Cristiano7988 <cristiano.morales@contractor.outroll.com>
2023-12-13 01:08:18 -0300

    Adição do Seeder de Status

---
+++ database/seeders/StatusSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('status')->insert([
+            ['tipo' => 'disponível'],
+            ['tipo' => 'vendido']
+        ]);
+    }
+}
